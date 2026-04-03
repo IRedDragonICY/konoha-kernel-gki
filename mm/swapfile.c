BayLibre/android_kernel_common

ANDROID: mm: Add vendor hook in swapon()
This patch add a vendor hook in swapon() for changing swap
info flags to meet the capability of certain swap devices.

Bug: 368512254
Change-Id: I009f039179db3d4ac23039c7ddd09fa852f3729f
Signed-off-by: Chinwen Chang <chinwen.chang@mediatek.com>

mm/swapfile.c
@@ -50,6 +50,7 @@
 #include "internal.h"
 #include "swap.h"
 #include <trace/hooks/bl_hib.h>
+#include <trace/hooks/mm.h>
 
 static bool swap_count_continued(struct swap_info_struct *, pgoff_t,
 				 unsigned char);
@@ -3499,6 +3500,8 @@ SYSCALL_DEFINE2(swapon, const char __user *, specialfile, int, swap_flags)
 
 	if (si->bdev && bdev_synchronous(si->bdev))
 		si->flags |= SWP_SYNCHRONOUS_IO;
+
+	trace_android_vh_adjust_swap_info_flags(&si->flags);
 
 	if (si->bdev && !hibernation_swap && bdev_nonrot(si->bdev)) {
 		si->flags |= SWP_SOLIDSTATE;

IRedDragonICY/konoha-kernel-gki

Bug: 491161822

mm/swapfile.c
@@ -50,6 +50,7 @@
 #include "internal.h"
 #include "swap.h"
 #include <trace/hooks/swapfile.h>
+#include <trace/hooks/mm.h>
 
 static bool swap_count_continued(struct swap_info_struct *, pgoff_t,
 				 unsigned char);
@@ -3496,6 +3497,8 @@ SYSCALL_DEFINE2(swapon, const char __user *, specialfile, int, swap_flags)
 
 	if (p->bdev && bdev_synchronous(p->bdev))
 		p->flags |= SWP_SYNCHRONOUS_IO;
+
+	trace_android_vh_adjust_swap_info_flags(&p->flags);
 
 	if (p->bdev && bdev_nonrot(p->bdev)) {
 		int cpu, i;